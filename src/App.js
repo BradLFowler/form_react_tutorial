afb3ca672a5d0217b6f7ae720123928a0a8a83bb
BradLFowler <BradLF00@gmail.com>
2022-07-27 22:16:38 -0500

    needs a database to post to

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    axios.post('/user', {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+    this.setState({
+      firstName: "",
+      lastName: ""
+    })
+  }
+
   render() {
     return (
-      <form>
-        
-      </form>
+      <div className="App">
+        <header className="App-header">
+          <p>Form in React</p>
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <input name="firstName" type="text" value={this.state.firstName} onChange={e => {this.handleChange(e)}} />
+            <input name="lastName" type="text" value={this.state.lastName} onChange={e => {this.handleChange(e)}} />
+            <input type="submit" />
+          </form>
+        </header>
+      </div>
     )
   }
 }
